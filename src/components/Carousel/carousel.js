6ed59526464a7f6c0657417552df3395eca3270f
Delvis <deapud@gmail.com>
2020-04-30 23:38:02 -0400

    Working on login styles

--- src/components/Carousel/carousel.js
+++ src/components/Carousel/carousel.js
@@ -1,33 +1,9 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-let styles = {
-    height: '100vh',
-    width: '50%'
-};
+import 'bootstrap/dist/css';
 
-const MyCarousel = () => {
-    return (
-        <div style={styles}>
 
-           <Carousel>
-                <div>
-                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
-                    <p className="legend">Hong Kong</p>
-                </div>
-                <div>
-                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore" />
-                    <p >hola jhghgy hjhjkuh uyuyukj,kmnb gtrt dgf</p>
-                </div>
-                <div>
-                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan" />
-                    <p className="legend">Japan</p>
-                </div>
-                      <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas" />
-                    <p className="legend">Las Vegas</p>
-                
-            </Carousel>
-        </div>
-    )
+const MyCarousel=()=> {
+    return (<div > <Carousel> <div> <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg"alt="Hong Kong"/> <p className="legend">Hong Kong</p> </div> <div> <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp"alt="Singapore"/> <p >hola jhghgy hjhjkuh uyuyukj, kmnb gtrt dgf</p> </div> <div> <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp"alt="Japan"/> <p className="legend">Japan</p> </div> <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp"alt="Las Vegas"/> <p className="legend">Las Vegas</p> </Carousel> </div>)
 }
+
 export default MyCarousel;
